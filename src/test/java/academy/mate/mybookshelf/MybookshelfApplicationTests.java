ca0b81242d4b5b1fbabe6b7dc185c6299e9730a4
roman_minevich <283389dv3qsh>
2019-11-07 15:44:55 +0200

    initial commit

---
+++ src/test/java/academy/mate/mybookshelf/MybookshelfApplicationTests.java
@@ -0,0 +1,13 @@
+package academy.mate.mybookshelf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybookshelfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
